c07dad0f3fb165753b99982b2366775fa9e1d7c1
Trần Minh Vũ <165811867+ZuMinnn@users.noreply.github.com>
2024-04-03 00:06:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -152,19 +152,29 @@ saveImgBtn.addEventListener("click", () => {
         saveImage(); 
     }, 100);
 });
-document.onkeydown = function(e) {
-    if(e.keyCode == 123) {
-        alert('Anh Zai Đừng Xài DevTool nhé tội bé!.');
+document.addEventListener('keydown', function(e) {
+    
+    if(e.keyCode === 123 || 
+       (e.ctrlKey && e.shiftKey && (e.keyCode === 'I'.charCodeAt(0) || e.keyCode === 'C'.charCodeAt(0) || e.keyCode === 'J'.charCodeAt(0))) || 
+       (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))) {
+        e.preventDefault(); 
+        showCustomAlert();
         return false;
     }
-    if(e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'U'.charCodeAt(0))) {
-        alert('Anh Zai Đừng Xài DevTool nhé tội bé!.');
-        return false;
-    }
-};
+}, false);
 
-// Check for F12, Ctrl+Shift+I/C/J/U
-window.oncontextmenu = function () {
-    alert('Anh Zai Đừng Xài DevTool nhé tội bé!');
-    return false; 
-};
+window.addEventListener('contextmenu', function(e) {
+    e.preventDefault(); 
+    showCustomAlert();
+    return false;
+}, false);
+
+function showCustomAlert() {
+    var alertBox = document.getElementById('customAlert');
+    alertBox.style.display = 'block';
+    
+    
+    setTimeout(function() {
+        alertBox.style.display = 'none';
+    }, 2000); 
+}
